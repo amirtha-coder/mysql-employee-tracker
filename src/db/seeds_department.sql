a597e3875a071aba5340ded5d2a202dad0708359
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-06-27 19:10:49 +0100

    seeds for dep & emp & role added

---
+++ src/db/seeds_department.sql
@@ -0,0 +1,4 @@
+INSERT INTO management_db.department(id, departmentName) VALUES ('1', 'HR');
+INSERT INTO management_db.department (id, departmentName) VALUES ('2', 'IT');
+INSERT INTO management_db. department (id, departmentName) VALUES ('3', 'Finance');
+INSERT INTO management_db. department (id, departmentName) VALUES ('4', 'Marketing');
